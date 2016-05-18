e50a75adfc28708d9fe0d46963d3db8691929e74
Yvalain <yvalainj@msn.com>
2016-05-18 07:18:26 +0200

    Java Programming Course 2 : Modifications to enhance the program and support lower case characters encryption

--- src/com/edu/yae/course2/CaesarCipher.java
+++ src/com/edu/yae/course2/CaesarCipher.java
@@ -8,19 +8,38 @@ public class CaesarCipher {
 
     public String encrypt(String input, int key) {
         StringBuilder encrypted = new StringBuilder(input);
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-        String shiftedAlphabet = alphabet.substring(key) + alphabet.substring(0, key);
 
         for (int i = 0; i < encrypted.length(); i++) {
             char currChar = encrypted.charAt(i);
-            int idx = alphabet.indexOf(currChar);
+            String alphabet = getAlphabetInGoodCase(currChar);
+            String shiftedAlphabet = getShiftedAlphabet(alphabet, key);
 
+            int idx = alphabet.indexOf(currChar);
             if (idx != -1) {
                 char newChar = shiftedAlphabet.charAt(idx);
                 encrypted.setCharAt(i, newChar);
             }
         }
         return encrypted.toString();
+    }
+
+    private String getShiftedAlphabet(String alphabet, int key) {
+        return alphabet.substring(key) + alphabet.substring(0, key);
+    }
+
+//    private String replaceInText(String alphabet, String text, char currChar) {
+//        int idx = alphabet.indexOf(currChar);
+//        if (idx != -1) {
+//            char newChar = shiftedAlphabet.charAt(idx);
+//            encrypted.setCharAt(i, newChar);
+//        return text;
+//    }
+
+    private String getAlphabetInGoodCase(char currChar) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if (Character.isLowerCase(currChar)) {
+            alphabet = alphabet.toLowerCase();
+        }
+        return alphabet;
     }
 }
--- src/com/edu/yae/course2/CaesarCipherTest.java
+++ src/com/edu/yae/course2/CaesarCipherTest.java
@@ -1,5 +1,6 @@
 package com.edu.yae.course2;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
@@ -9,6 +10,15 @@ import java.io.*;
  * Created by yvalain on 16/05/2016.
  */
 public class CaesarCipherTest {
+    private String filePath;
+    private int key;
+
+    @Before
+    public void executedBeforeEach() {
+        filePath = "/Users/yvalain/MyProjects/Java/JavaProgrammingCourse2/test/message3.txt";
+        key = 17;
+    }
+
 
     public String getFileText(String filePath) {
         String line;
@@ -33,10 +43,8 @@ public class CaesarCipherTest {
 
     @Test
     public void testCaesar() {
-        String filePath = "/Users/yvalain/MyProjects/Java/JavaProgrammingCourse2/test/message1.txt";
         CaesarCipher caesarCipher = new CaesarCipher();
         String message = getFileText(filePath);
-        int key = 17;
         String encrypted = caesarCipher.encrypt(message, key);
         System.out.println("Encrypted: " + encrypted);
         String decrypted = caesarCipher.encrypt(encrypted, 26 - key);
